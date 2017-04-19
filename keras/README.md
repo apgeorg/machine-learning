6ec95276b4f618ad943d474a931cc0084732ba20
Apostolos Georgiadis <apostolos.georgiadis@gmx.ch>
2017-04-19 12:08:27 +0200

    cat or dog example added.

--- keras/README.md
+++ keras/README.md
@@ -1,14 +1,7 @@
 # Keras Examples
 
-## xor
-Simple XOR example in Keras.
+## Cat or Dog ?
+A simple Convolutional Neural Networks (CNN) for image classification. 
 
-## flowers 
-This example shows how you can build and train deep convolution neural networks (DCNN) on flower photos in Keras. 
-You can download the dataset either by executing the script below:
-```bash
-$ ./install.sh
-```
-or manually from https://www.tensorflow.org/versions/r0.9/how_tos/image_retraining/index.html
 
 
---
+++ keras/catdog/cnn.py
@@ -0,0 +1,53 @@
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+
+# Initialising the CNN
+model = Sequential()
+
+# 1st - Convolution layer with Pooling layer 
+model.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# 2nd - Convolution layer with Pooling layer 
+model.add(Convolution2D(32, 3, 3, activation = 'relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Flattening
+model.add(Flatten())
+
+# Full connection
+model.add(Dense(128, activation = 'relu'))
+model.add(Dense(1, activation = 'sigmoid'))
+
+# Compiling the CNN
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the CNN to the images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+                                            
+model.fit_generator(training_set,
+                         samples_per_epoch=8000,
+                         nb_epoch = 25,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)
+
