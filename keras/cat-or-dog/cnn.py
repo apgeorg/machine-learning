be16e46c96d89ef952245166a9fc073129a23882
Apostolos Georgiadis <apostolos.georgiadis@gmx.ch>
2017-04-19 17:07:55 +0200

    rename folder to cat-or-dog

--- keras/cat-or-dog/cnn.py
+++ keras/cat-or-dog/cnn.py
@@ -28,12 +28,14 @@ model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accu
 # Fitting the CNN to the images
 from keras.preprocessing.image import ImageDataGenerator
 
+
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    zoom_range = 0.2,
                                    horizontal_flip = True)
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
+
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
