4fdc0cbdeef4244bc6327fbf605c2b0473dbe440
Apostolos Georgiadis <apostolos.georgiadis@gmx.ch>
2017-05-08 07:47:32 +0200

    Model architecture which reaches over 92%

--- keras/cat-or-dog/cnn.py
+++ keras/cat-or-dog/cnn.py
@@ -1,11 +1,11 @@
 # Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 
 # Image dimensions
-img_width, img_height = 128, 128 
-
+img_width, img_height = 150, 150 
 
 """
     Creates a CNN model
@@ -24,6 +24,9 @@ def create_model(p, input_shape=(32, 32, 3)):
     # Convolution + Pooling Layer 
     model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    # Convolution + Pooling Layer 
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     
     # Flattening
     model.add(Flatten())
@@ -31,11 +34,12 @@ def create_model(p, input_shape=(32, 32, 3)):
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(p))
     model.add(Dense(64, activation='relu'))
+    model.add(Dense(64, activation='relu'))
     model.add(Dropout(p/2))
     model.add(Dense(1, activation='sigmoid'))
     
     # Compiling the CNN
-    optimizer = 'adam'
+    optimizer = Adam(lr=1e-3)
     metrics=['accuracy']
     model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=metrics)
     return model
@@ -61,7 +65,7 @@ def run_training(bs=32, epochs=10):
                                             batch_size = bs,
                                             class_mode = 'binary')
                                             
-    model = create_model(p=0.5, input_shape=(img_width, img_height, 3))                                  
+    model = create_model(p=0.6, input_shape=(img_width, img_height, 3))                                  
     model.fit_generator(training_set,
                          steps_per_epoch=8000/bs,
                          epochs = epochs,
@@ -69,7 +73,7 @@ def run_training(bs=32, epochs=10):
                          validation_steps = 2000/bs)
 
 def main():
-    run_training(bs=32, epochs=50)
+    run_training(bs=32, epochs=100)
 
 """ Main """
 if __name__ == "__main__":
